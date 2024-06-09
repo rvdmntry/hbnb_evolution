00c0c79786d68a48bacf5b86f46ef8ccf661db02
ramon <r.antovega@gmail.com>
2024-06-09 15:05:07 -0400

    place

--- models/place.py
+++ models/place.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
-"""places"""
+import uuid
+from datetime import datetime
 
 
-from model.base_model import BaseModel
-
-class Place(BaseModel):
-    def __init__(self, name, description, address, city_id, latitude, longitude, host_id, number_of_rooms, number_of_bathrooms, price_per_night, max_guests, amenities=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class Place:
+    def __init__(self, name, description, address, city_id, latitude, longitude, host_id, number_of_rooms, number_of_bathrooms, price_per_night, max_guests, amenity_ids):
+        self.id = str(uuid.uuid4())
         self.name = name
         self.description = description
         self.address = address
@@ -18,4 +16,30 @@ class Place(BaseModel):
         self.number_of_bathrooms = number_of_bathrooms
         self.price_per_night = price_per_night
         self.max_guests = max_guests
-        self.amenities = amenities if amenities is not None else []
+        self.amenity_ids = amenity_ids
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
+
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'address': self.address,
+            'city_id': self.city_id,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'host_id': self.host_id,
+            'number_of_rooms': self.number_of_rooms,
+            'number_of_bathrooms': self.number_of_bathrooms,
+            'price_per_night': self.price_per_night,
+            'max_guests': self.max_guests,
+            'amenity_ids': self.amenity_ids,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
