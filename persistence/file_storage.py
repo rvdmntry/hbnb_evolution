973ec18a2d940f76e55bc2be8804627a0b5ec800
ramon <r.antovega@gmail.com>
2024-06-10 12:12:44 -0400

    updatepers

--- persistence/file_storage.py
+++ persistence/file_storage.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-"""
-File Storage module
-"""
-
 import json
 import os
 
@@ -12,17 +7,26 @@ class FileStorage:
     __objects = {}
 
     @classmethod
-    def save(cls, obj):
-        cls.__objects[obj.id] = obj.to_dict()
-        with open(cls.__file_path, 'w') as f:
-            json.dump(cls.__objects, f)
+    def all(cls):
+        return cls.__objects
 
     @classmethod
-    def load(cls):
+    def save(cls):
+        with open(cls.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in cls.__objects.items()}, f)
+
+    @classmethod
+    def reload(cls):
         if os.path.exists(cls.__file_path):
             with open(cls.__file_path, 'r') as f:
                 cls.__objects = json.load(f)
 
     @classmethod
-    def get(cls, obj_id):
-        return cls.__objects.get(obj_id)
+    def new(cls, obj):
+        cls.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
+
+    @classmethod
+    def delete(cls, obj):
+        key = obj.__class__.__name__ + '.' + obj.id
+        if key in cls.__objects:
+            del cls.__objects[key]
